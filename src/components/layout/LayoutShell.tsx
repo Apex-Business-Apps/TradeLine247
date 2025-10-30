c5867e73b0e25b19b628bc0f9206e0be205df30f
Apex Business Apps <jrmendozaceo@apexbusiness-systems.com>
2025-10-30 00:35:33 -0600

    Create LayoutShell.tsx

---
+++ src/components/layout/LayoutShell.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+// If you have a Footer component, import it here:
+// import Footer from "./Footer";
+
+export default function LayoutShell() {
+  return (
+    <>
+      <Header />
+      <main id="content" className="min-h-[60vh]">
+        <Outlet />
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
+}
