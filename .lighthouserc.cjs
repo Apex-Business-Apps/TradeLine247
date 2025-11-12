9aa8506a21fdaad2a07ad56c42c607c71400b1b7
Apex Business Apps <s4784f7nrc@privaterelay.appleid.com>
2025-11-12 03:14:40 -0700

    ci: split db/app deploy; supabase non-interactive; vercel stateless; lhci non-blocking

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -1,23 +1,18 @@
+/** @type {import('lighthouse-ci').LHCI.Config} */
 module.exports = {
   ci: {
-    collect: {
-      startServerCommand: null,
-      url: ['http://localhost:43073/'],
-      numberOfRuns: 1,
-    },
+    collect: { startServerCommand: null, url: ['http://localhost:43073/'], numberOfRuns: 1 },
     assert: {
       preset: 'lighthouse:recommended',
       assertions: {
         'color-contrast': 'off',
-        'categories:performance': ['warn', { minScore: 0.35 }],
-        'render-blocking-resources': ['warn', { minScore: 0 }],
-        'unused-css-rules': ['warn', { minScore: 0.5 }],
-        'unused-javascript': ['warn', { minScore: 0 }],
-        'uses-responsive-images': ['warn', { minScore: 0 }],
+        'categories:performance': ['warn', {minScore: 0.35}],
+        'render-blocking-resources': ['warn', {minScore: 0}],
+        'unused-css-rules': ['warn', {minScore: 0.5}],
+        'unused-javascript': ['warn', {minScore: 0}],
+        'uses-responsive-images': ['warn', {minScore: 0}],
       },
     },
-    upload: {
-      target: 'temporary-public-storage',
-    },
+    upload: { target: 'temporary-public-storage' }
   },
 };
